8518c47f49d5a8ee665c96bbac184ad70bd73b84
lollileeno <160266590+lollileeno@users.noreply.github.com>
2025-02-22 18:21:16 +0300

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Runner {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		 int pID = 0;
+		 int arriveT = 0;
+		 int burstT;
+		 boolean c = true;
+		 do {
+			 
+			 
+		 }while(c);
+		 
+
+	}
+
+}
