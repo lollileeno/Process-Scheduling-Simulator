7d32af7ecd1f1255aad5636682f2591e56c4c85f
lollileeno <160266590+lollileeno@users.noreply.github.com>
2025-02-22 18:12:37 +0300

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,19 @@
+
+public class Process {
+	int processID;
+	int arrivalTime;
+	int burstTime;
+	int completionTime;
+	int waitingTime;
+	int turnaroundTime;
+	public Process(int processID, int arrivalTime, int burstTime) {
+		this.processID = processID;
+		this.arrivalTime = arrivalTime;
+		this.burstTime = burstTime;
+		
+	}
+	
+
+	
+
+}
